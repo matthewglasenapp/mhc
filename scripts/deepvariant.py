a69e09f0b142861c2e97cd26c0234e389a50999a
Matthew Glasenapp <mglasena@ucsc.edu>
2024-12-12 09:17:13 -0800

    update

--- scripts/deepvariant.py
+++ scripts/deepvariant.py
@@ -3,11 +3,25 @@ import os
 wd = "/hb/scratch/ogarci12/deepvariant"
 # Revio data with duplicates removed
 input_dir = "/hb/scratch/ogarci12/PacBio_mm2/rm_dup/"
-promethion_input_dir = ""
+
+# Revio Output Dir
 output_dir = "/hb/scratch/ogarci12/deepvariant/Revio_VCF/"
+
+# PromethION BAM with Duplicates Removed
+promethion_input_dir = "/hb/scratch/mglasena/deepvariant_whathap/Promethion_mm2/mapped_raw/"
+
+# PromethION/Clair3 output dir
+clair3_output_dir = "/hb/scratch/mglasena/deepvariant_whathap/PromethION_VCF/"
+
+# Output dir for whatshap
 phased_bam_dir = "/hb/scratch/ogarci12/deepvariant/Revio_Phased_BAM"
+
+# Reference fasta
 ref = "/hb/scratch/ogarci12/deepvariant/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa"
+
 deepvariant_sif = "/hb/scratch/ogarci12/deepvariant/deepvariant.sif"
+clair3_sif = "/hb/scratch/mglasena/deepvariant_whathap/clair3_latest.sif"
+clair3_model_path = "/hb/home/mglasena/.conda/envs/clair3/bin/models/r941_prom_sup_g5014"
 
 # Threads for Clair3
 threads = 8
@@ -31,13 +45,12 @@ def run_deepvariant(sample):
     os.system(run_deepvariant)
 
 def run_clair3(sample):
-    input_file = promethion_input_dir + sample + ".hg.38.bam"
+    input_file = promethion_input_dir + sample + ".hg38.minimap.promethion.raw.bam"
+    print("Variant Calling {}".format(input_file))
     index = "samtools index {}".format(input_file)
     os.system(index)
-    
-    clair_model_path = ""
-    run_clair3 = "run_clair3.sh --bam_fn={} --ref_fn={} --threads={} --platform=ont --model_path={} --output={}".format(input_file, sample, ref, threads, output_dir)
 
+    run_clair3 = "run_clair3.sh --bam_fn={} --ref_fn={} --platform=ont --model_path={} --output={} --threads={}".format(input_file, ref, clair3_model_path, clair3_output_dir, threads)
     os.system(run_clair3)
 
 def index_vcf(sample):
@@ -66,6 +79,7 @@ def main():
     array_id = os.environ["array_id"]
     print("Array ID: {}".format(array_id))
     sample = samples[int(array_id)]
+    print(sample)
     #index_bam(sample)
     #run_deepvariant(sample)
     run_clair3(sample)
