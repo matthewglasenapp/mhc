6b091318068a6889a485c45157cf2c53782c7ab2
Matthew Glasenapp <mglasena@ucsc.edu>
2025-01-09 10:50:55 -0800

    update

--- scripts/get_prop_on_target.py
+++ scripts/get_prop_on_target.py
@@ -2,52 +2,61 @@ import os
 import pysam
 import pybedtools
 import statistics
+import csv
 
-mapped_bam_dir = "/hb/scratch/mglasena/MHC/mapped_bam/revio/"
+platforms = ["revio", "promethion"]
+
+mapped_bam_dir = "/hb/scratch/mglasena/MHC/mapped_bam/"
 output_directory = "/hb/scratch/mglasena/MHC/on_target/"
-#bed_file = "/hb/scratch/mglasena/MHC/on_target/mhc.bed"
 bed_file = "/hb/scratch/mglasena/MHC/on_target/expected_coverage.bed"
 
+sample_list = [
+    'HG002', 'HG003', 'HG004', 'HG005', 'HG01106', 'HG01258', 'HG01928', 'HG02055', 'HG02630', 'HG03492',
+    'HG03579', 'NA19240', 'NA20129', 'NA21309', 'NA24694', 'NA24695', 'HG01891', 'IHW09021', 'IHW09049',
+    'IHW09071', 'IHW09117', 'IHW09118', 'IHW09122', 'IHW09125', 'IHW09175', 'IHW09198', 'IHW09200',
+    'IHW09224', 'IHW09245', 'IHW09251', 'IHW09359', 'IHW09364', 'IHW09409'
+]
+
+data_dict = {sample: {} for sample in sample_list}
+
+
+def calculate_total_target_length(bed_file):
+    return sum(
+        int(feature.end) - int(feature.start) for feature in pybedtools.BedTool(bed_file)
+    )
+
+
 class BAMStatistics:
-    def __init__(self, sample):
+    def __init__(self, sample, platform, total_target_length):
         self.sample = sample
-        self.bam_file = mapped_bam_dir + sample + ".hg38.revio_marked_duplicates.bam"
-        self.output_directory = output_directory + sample + "/"
+        self.platform = platform
+        self.total_target_length = total_target_length
+        self.bam_file = mapped_bam_dir + platform + "/" + sample + ".hg38." + platform + "_marked_duplicates.bam"
+        self.output_directory = output_directory + platform + "/" + sample + "/"
         os.system("mkdir -p {}".format(self.output_directory))
 
         # Initialize variables
         self.total_reads = 0
         self.total_bases = 0
         self.all_read_lengths = []
-
         self.on_target_bases = 0
-        self.total_target_length = 0
         self.bed_read_lengths = []
         self.overlapping_read_count = 0
         self.average_on_target_depth = 0
 
     def filter_bam(self):
-        # Filter BAM for primary alignments, MAPQ ≥ 20, and non-duplicates
         filtered_bam_file = self.output_directory + self.sample + "_filtered.bam"
         filter_bam = "samtools view -h -q 20 -F 0x4 -F 0x100 -F 0x400 -F 0x800 {} | samtools view -b > {}".format(self.bam_file, filtered_bam_file)
-        index_filtered_bam = "samtools index {}".format(filtered_bam_file)
         os.system(filter_bam)
-        os.system(index_filtered_bam)
+        os.system("samtools index {}".format(filtered_bam_file))
 
-        # Calculate total reads and their lengths for the filtered BAM
         with pysam.AlignmentFile(filtered_bam_file, "rb") as bamfile:
             for read in bamfile:
                 self.total_reads += 1
                 self.total_bases += read.query_alignment_length
                 self.all_read_lengths.append(read.query_length)
 
-        # Calculate total target region length
-        self.total_target_length = sum(
-            int(feature.end) - int(feature.start) for feature in pybedtools.BedTool(bed_file)
-        )
-
     def intersect_bam_with_targets(self):
-        # Intersect filtered BAM with target regions
         filtered_bam_file = self.output_directory + self.sample + "_filtered.bam"
         intersect_output_file = self.output_directory + self.sample + "_on_target.bam"
         intersect = "bedtools intersect -abam {} -b {} > {}".format(filtered_bam_file, bed_file, intersect_output_file)
@@ -56,7 +65,6 @@ class BAMStatistics:
         os.system(index)
 
     def calculate_statistics(self):
-        # Calculate statistics for the intersected BAM
         intersected_bam_file = self.output_directory + self.sample + "_on_target.bam"
         with pysam.AlignmentFile(intersected_bam_file, "rb") as bamfile:
             for read in bamfile:
@@ -64,41 +72,66 @@ class BAMStatistics:
                 self.bed_read_lengths.append(read.query_length)
                 self.on_target_bases += read.query_alignment_length
 
-        # Calculate average on-target depth
         if self.total_target_length > 0:
             self.average_on_target_depth = self.on_target_bases / self.total_target_length
 
-    def print_results(self):
-        # Proportion calculations
         proportion_on_target_bases = self.on_target_bases / self.total_bases if self.total_bases > 0 else 0
         proportion_reads_overlapping = self.overlapping_read_count / self.total_reads if self.total_reads > 0 else 0
-
-        # Median read lengths
         median_all_reads = statistics.median(self.all_read_lengths) if self.all_read_lengths else 0
         median_bed_reads = statistics.median(self.bed_read_lengths) if self.bed_read_lengths else 0
 
-        # Print results
-        print("Total Target Region Length: {} bp".format(self.total_target_length))
-        print("Total Reads in BAM: {}".format(self.total_reads))
-        print("Reads overlapping target region(s): {}".format(self.overlapping_read_count))
-        print("Proportion of Reads Mapping to Target Region: {:.6f}".format(proportion_reads_overlapping))
-        print("Median Read Length (All Reads): {}".format(median_all_reads))
-        print("Median Read Length (BED-Overlapping Reads): {}".format(median_bed_reads))
-        print("Total Aligned Bases: {}".format(self.total_bases))
-        print("Total On-Target Bases: {}".format(self.on_target_bases))
-        print("Proportion On-Target: {:.6f}".format(proportion_on_target_bases))
-        print("Average On-Target Depth: {:.2f}X".format(self.average_on_target_depth))
+        data_dict[self.sample] = {
+            "Total Reads": self.total_reads,
+            "Reads Overlapping Target Region": self.overlapping_read_count,
+            "Proportion of Reads Mapping to Target Region": proportion_reads_overlapping,
+            "Median Read Length (All Reads)": median_all_reads,
+            "Median Read Length (BED-Overlapping Reads)": median_bed_reads,
+            "Total Aligned Bases": self.total_bases,
+            "On-Target Bases": self.on_target_bases,
+            "Proportion On-Target": proportion_on_target_bases,
+            "Average On-Target Depth": self.average_on_target_depth
+        }
+
+
+def write_results(csv_file):
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            "Sample", "Total Reads", "Reads Overlapping Target Region",
+            "Proportion of Reads Mapping to Target Region", "Median Read Length (All Reads)",
+            "Median Read Length (BED-Overlapping Reads)", "Total Aligned Bases", "On-Target Bases",
+            "Proportion On-Target", "Average On-Target Depth"
+        ])
+        for sample, stats in data_dict.items():
+            writer.writerow([
+                sample,
+                stats.get("Total Reads", 0),
+                stats.get("Reads Overlapping Target Region", 0),
+                stats.get("Proportion of Reads Mapping to Target Region", 0),
+                stats.get("Median Read Length (All Reads)", 0),
+                stats.get("Median Read Length (BED-Overlapping Reads)", 0),
+                stats.get("Total Aligned Bases", 0),
+                stats.get("On-Target Bases", 0),
+                stats.get("Proportion On-Target", 0),
+                stats.get("Average On-Target Depth", 0)
+            ])
+
 
 def main():
-    sample_name = "HG002"
-    
-    sample = BAMStatistics(sample_name)
-    sample.filter_bam()
-    sample.intersect_bam_with_targets()
-    sample.calculate_statistics()
-    sample.print_results()
+    total_target_length = calculate_total_target_length(bed_file)
+
+    for platform in platforms:
+        csv_file = output_directory + platform + "/results.csv"
+
+        for sample_name in sample_list:
+            sample = BAMStatistics(sample_name, platform, total_target_length)
+            sample.filter_bam()
+            sample.intersect_bam_with_targets()
+            sample.calculate_statistics()
+
+        write_results(csv_file)
+
 
 if __name__ == "__main__":
     main()
-
 
