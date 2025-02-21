b6da5250e1e0e7104c2e20a829bdfa3480ba7c46
Matthew Glasenapp <mglasena@ucsc.edu>
2025-02-20 17:24:48 -0800

    update

--- scripts/hg002_sv_benchmark.sh
+++ scripts/hg002_sv_benchmark.sh
@@ -16,17 +16,18 @@ truth_vcf="/hb/scratch/mglasena/MHC/concordance/GIAB_benchmark/HG002_SV/NIST_SV_
 query_vcf="/hb/scratch/mglasena/test_pacbio/processed_data/GRCh37/pbsv_vcf/HG002.dedup.trimmed.hg37.chr6.SV.vcf.gz"
 confident_regions="/hb/scratch/mglasena/MHC/concordance/GIAB_benchmark/HG002_SV/NIST_SV_v0.6/HG002_SVs_Tier1_v0.6.bed"
 regions_file="/hb/scratch/mglasena/test_pacbio/merged_hla.bed"
-reference_fasta="/hb/scratch/mglasena/test_pacbio/reference/GRCh37/hs37d5.fa.gz"
+reference_fasta="/hb/scratch/mglasena/test_pacbio/reference/GRCh37/hs37d5.fa"
 output_prefix="hap_py_results/HG002"
 rtg_path="/hb/home/mglasena/.conda/envs/happy/bin/rtg"
-rtg_template="/hb/scratch/mglasena/MHC/concordance/hap_py_input/rtg_sdf_template"
+rtg_template="/hb/scratch/mglasena/MHC/concordance/hap_py_input/rtg_sdf_grch37"
+"
 threads=8
 
 export HGREF=$reference_fasta
 
 # Don't use -f $confident_regions
 
-hap.py $truth_vcf $query_vcf  -R $regions_file -r $reference_fasta -o $output_prefix --engine vcfeval --engine-vcfeval-path $rtg_path --engine-vcfeval-template $rtg_template --threads $threads
+hap.py $truth_vcf $query_vcf -R $regions_file -r $reference_fasta -o $output_prefix --engine vcfeval --engine-vcfeval-path $rtg_path --engine-vcfeval-template $rtg_template --threads $threads
 
 #SV Analyzer
 # https://github.com/nhansen/SVanalyzer/blob/master/docs/svbenchmark.rst
