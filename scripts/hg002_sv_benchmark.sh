538bea482523796ea3d83d503c946479da1e28f3
Matthew Glasenapp <mglasena@ucsc.edu>
2025-02-20 17:25:09 -0800

    update

--- scripts/hg002_sv_benchmark.sh
+++ scripts/hg002_sv_benchmark.sh
@@ -13,14 +13,13 @@
 #SBATCH --account=pi-jkoc
 
 truth_vcf="/hb/scratch/mglasena/MHC/concordance/GIAB_benchmark/HG002_SV/NIST_SV_v0.6/HG002_SVs_Tier1_v0.6.vcf.gz"
-query_vcf="/hb/scratch/mglasena/test_pacbio/processed_data/GRCh37/pbsv_vcf/HG002.dedup.trimmed.hg37.chr6.SV.vcf.gz"
+query_vcf="/hb/scratch/mglasena/test_pacbio/processed_data/GRCh37/pbsv_vcf/HG002.dedup.trimmed.hg37.chr6.SV.norm.vcf.gz"
 confident_regions="/hb/scratch/mglasena/MHC/concordance/GIAB_benchmark/HG002_SV/NIST_SV_v0.6/HG002_SVs_Tier1_v0.6.bed"
-regions_file="/hb/scratch/mglasena/test_pacbio/merged_hla.bed"
+regions_file="/hb/scratch/mglasena/test_pacbio/merged_hla_2.bed"
 reference_fasta="/hb/scratch/mglasena/test_pacbio/reference/GRCh37/hs37d5.fa"
 output_prefix="hap_py_results/HG002"
 rtg_path="/hb/home/mglasena/.conda/envs/happy/bin/rtg"
 rtg_template="/hb/scratch/mglasena/MHC/concordance/hap_py_input/rtg_sdf_grch37"
-"
 threads=8
 
 export HGREF=$reference_fasta
